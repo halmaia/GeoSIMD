ebd197ac65f701387f67e0899556adad46fe4203
Ákos Halmai <halmaia@gmail.com>
2024-03-13 12:13:00 +0100

    Wmerc prototype

--- GeoSIMD.cs
+++ GeoSIMD.cs
@@ -414,108 +414,206 @@ namespace GeoSIMD
             return min;
         }
 
-        public static (double minX, double minY, double maxX, double maxY) Envelope(ReadOnlySpan<double> x, ReadOnlySpan<double> y)
-        {
-            nuint bufferLength = (nuint)x.Length;
+        //public static (double minX, double minY, double maxX, double maxY) Envelope(ReadOnlySpan<double> x, ReadOnlySpan<double> y)
+        //{
+        //    nuint bufferLength = (nuint)x.Length;
 
+        //    const nuint elementCountVector512 = 8u;
+        //    const nuint elementCountVector256 = 4u;
+        //    const nuint elementCountVector128 = 2u;
+
+        //    // Vector512/256/128.IsHardwareAccelerated is a compilation time constant.
+        //    if (Vector512.IsHardwareAccelerated && bufferLength >= elementCountVector512)
+        //    {
+
+        //        ref readonly double searchSpaceX = ref x.GetPinnableReference();
+        //        ref readonly double searchSpaceY = ref y.GetPinnableReference();
+
+        //        Vector512<double> minX512 = Vector512.LoadUnsafe(in searchSpaceX);
+        //        Vector512<double> minY512 = Vector512.LoadUnsafe(in searchSpaceY);
+
+        //        Vector512<double> maxX512 = minX512;
+        //        Vector512<double> maxY512 = minY512;
+
+        //        nuint elementOffset = elementCountVector512;
+        //        nuint oneVectorAwayFromEnd = bufferLength - elementCountVector512;
+
+        //        while (elementOffset <= oneVectorAwayFromEnd)
+        //        {
+        //            var currentX = Vector512.LoadUnsafe(in searchSpaceX, elementOffset);
+        //            minX512 = Vector512.Min(minX512, currentX);
+        //            maxX512 = Vector512.Max(maxX512, currentX);
+
+        //            var currentY = Vector512.LoadUnsafe(in searchSpaceY, elementOffset);
+        //            minY512 = Vector512.Min(minY512, currentY);
+        //            maxY512 = Vector512.Max(maxY512, currentY);
+
+        //            elementOffset += elementCountVector512;
+        //        }
+
+        //        if (elementOffset < bufferLength)
+        //        {
+        //            minX512 = Vector512.Min(minX512, Vector512.LoadUnsafe(in searchSpaceX, oneVectorAwayFromEnd));
+        //        }
+
+        //        Vector256<double> min256 = Vector256.Min(minX512.GetLower(), minX512.GetUpper());
+        //        Vector128<double> min128 = Vector128.Min(min256.GetLower(), min256.GetUpper());
+        //        double right = min128[1], left = min128[0];
+        //        return left < right ? left : right;
+
+        //    }
+        //    else if (Vector256.IsHardwareAccelerated && bufferLength >= elementCountVector256)
+        //    {
+        //        ref readonly double searchSpace = ref x.GetPinnableReference();
+        //        Vector256<double> min256 = Vector256.LoadUnsafe(in searchSpace);
+        //        nuint elementOffset = elementCountVector256;
+        //        nuint oneVectorAwayFromEnd = bufferLength - elementCountVector256;
+        //        while (elementOffset <= oneVectorAwayFromEnd)
+        //        {
+        //            min256 = Vector256.Min(min256, Vector256.LoadUnsafe(in searchSpace, elementOffset));
+        //            elementOffset += elementCountVector256;
+        //        }
+
+        //        if (elementOffset < bufferLength)
+        //        {
+        //            min256 = Vector256.Min(min256, Vector256.LoadUnsafe(in searchSpace, oneVectorAwayFromEnd));
+        //        }
+
+        //        Vector128<double> min128 = Vector128.Min(min256.GetLower(), min256.GetUpper());
+        //        double right = min128[1], left = min128[0];
+        //        return left < right ? left : right;
+        //    }
+        //    else if (Vector128.IsHardwareAccelerated && bufferLength >= elementCountVector128)
+        //    {
+
+        //        ref readonly double searchSpace = ref x.GetPinnableReference();
+        //        Vector128<double> min128 = Vector128.LoadUnsafe(in searchSpace);
+        //        nuint elementOffset = elementCountVector128;
+        //        nuint oneVectorAwayFromEnd = bufferLength - elementCountVector128;
+        //        while (elementOffset <= oneVectorAwayFromEnd)
+        //        {
+        //            min128 = Vector128.Min(min128, Vector128.LoadUnsafe(in searchSpace, elementOffset));
+        //            elementOffset += elementCountVector128;
+        //        }
+
+        //        if (elementOffset < bufferLength)
+        //        {
+        //            min128 = Vector128.Min(min128, Vector128.LoadUnsafe(in searchSpace, oneVectorAwayFromEnd));
+        //        }
+
+        //        double right = min128[1], left = min128[0];
+        //        return left < right ? left : right;
+        //    }
+
+        //    // Non-SIMD scalar handling:
+        //    double min = x[0];
+        //    for (int i = 1; i < (int)bufferLength;)
+        //    {
+        //        double current = x[i++];
+        //        if (min > current)
+        //        {
+        //            min = current;
+        //        }
+        //    }
+        //    return min;
+        //}
+
+        [SkipLocalsInit]
+        public static void WebMercator(Span<double> lon, Span<double> lat)
+        {
             const nuint elementCountVector512 = 8u;
             const nuint elementCountVector256 = 4u;
             const nuint elementCountVector128 = 2u;
 
-            // Vector512/256/128.IsHardwareAccelerated is a compilation time constant.
+            const double s1 = 0.9999999999999375599318186670190419967059614976305799594333d;
+            const double s2 = -0.1666666666643233145818157423824897486264256985983593141745d;
+            const double s3 = 0.0083333333187655140151317116116415304845125161193612244296d;
+            const double s4 = -0.0001984126641162215009836522006852359604674565106031826592d;
+            const double s5 = 2.7556931926594908040601767274775207243915634258173167E-6d;
+            const double s6 = -2.50295188656032073490380438676828409194568629418124E-8d;
+            const double s7 = 1.540117037141464425086633145773563888660980948457E-10d;
+
+            const double maxLat = 85.051128779806592377796715521924692066982591268420688405762459391d;
+            const double minLat = -maxLat;
+
+            const double WGS84_SemiMajorAxis = 6378137.0d;
+            const double WGS84_HalfSemiMajorAxis = 3189068.5d;
+            const double degToRad = 0.0174532925199432957692369076848861271344287188854172545609719144d;
+
+
+            nuint elementOffset = 0u;
+            nuint bufferLength = (nuint)lon.Length;
+
+            ref double lon_searchSpace = ref MemoryMarshal.GetReference(lon);
+
             if (Vector512.IsHardwareAccelerated && bufferLength >= elementCountVector512)
             {
-
-                ref readonly double searchSpaceX = ref x.GetPinnableReference();
-                ref readonly double searchSpaceY = ref y.GetPinnableReference();
-
-                Vector512<double> minX512 = Vector512.LoadUnsafe(in searchSpaceX);
-                Vector512<double> minY512 = Vector512.LoadUnsafe(in searchSpaceY);
-
-                Vector512<double> maxX512 = minX512;
-                Vector512<double> maxY512 = minY512;
-
-                nuint elementOffset = elementCountVector512;
-                nuint oneVectorAwayFromEnd = bufferLength - elementCountVector512;
-
-                while (elementOffset <= oneVectorAwayFromEnd)
-                {
-                    var currentX = Vector512.LoadUnsafe(in searchSpaceX, elementOffset);
-                    minX512 = Vector512.Min(minX512, currentX);
-                    maxX512 = Vector512.Max(maxX512, currentX);
-
-                    var currentY = Vector512.LoadUnsafe(in searchSpaceY, elementOffset);
-                    minY512 = Vector512.Min(minY512, currentY);
-                    maxY512 = Vector512.Max(maxY512, currentY);
-
-                    elementOffset += elementCountVector512;
-                }
-
-                if (elementOffset < bufferLength)
-                {
-                    minX512 = Vector512.Min(minX512, Vector512.LoadUnsafe(in searchSpaceX, oneVectorAwayFromEnd));
-                }
-
-                Vector256<double> min256 = Vector256.Min(minX512.GetLower(), minX512.GetUpper());
-                Vector128<double> min128 = Vector128.Min(min256.GetLower(), min256.GetUpper());
-                double right = min128[1], left = min128[0];
-                return left < right ? left : right;
-
+              
+                
             }
             else if (Vector256.IsHardwareAccelerated && bufferLength >= elementCountVector256)
             {
-                ref readonly double searchSpace = ref x.GetPinnableReference();
-                Vector256<double> min256 = Vector256.LoadUnsafe(in searchSpace);
-                nuint elementOffset = elementCountVector256;
                 nuint oneVectorAwayFromEnd = bufferLength - elementCountVector256;
-                while (elementOffset <= oneVectorAwayFromEnd)
+
+                Vector256<double> v_s1 = Vector256.Create<double>(s1);
+                Vector256<double> v_s2 = Vector256.Create<double>(s2);
+                Vector256<double> v_s3 = Vector256.Create<double>(s3);
+                Vector256<double> v_s4 = Vector256.Create<double>(s4);
+                Vector256<double> v_s5 = Vector256.Create<double>(s5);
+                Vector256<double> v_s6 = Vector256.Create<double>(s6);
+                Vector256<double> v_s7 = Vector256.Create<double>(s7);
+
+                Vector256<double> v_minLat = Vector256.Create<double>(minLat);
+                Vector256<double> v_maxLat = Vector256.Create<double>(maxLat);
+                Vector256<double> v_one = Vector256<double>.One;
+                Vector256<double> v_two = Vector256.Create<double>(2.0d);
+
+                Vector256<double> v_degToRad = Vector256.Create<double>(degToRad);
+
+                Vector256<double> v_WGS84_HalfSemiMajorAxis = Vector256.Create<double>(WGS84_HalfSemiMajorAxis);
+                Vector256<double> v_WGS84_SemiMajorAxis = Vector256.Create<double>(WGS84_SemiMajorAxis);
+
+                while (true)
                 {
-                    min256 = Vector256.Min(min256, Vector256.LoadUnsafe(in searchSpace, elementOffset));
+                    Vector256<double> _lat = Vector256.LoadUnsafe(in lon_searchSpace, elementOffset);
+                    _lat = v_degToRad * Vector256.Min(v_maxLat, Vector256.Max(v_minLat, _lat));
+                    Vector256<double> _lat2 = _lat * _lat;
+
+                    _lat = Fma.IsSupported ?
+                    Fma.MultiplyAdd(_lat,
+                        Fma.MultiplyAdd(_lat2,
+                        Fma.MultiplyAdd(_lat2,
+                        Fma.MultiplyAdd(_lat2,
+                        Fma.MultiplyAdd(_lat2,
+                        Fma.MultiplyAdd(_lat2,
+                        Fma.MultiplyAdd(v_s7, _lat2, v_s6), v_s5), v_s4), v_s3), v_s2), v_s1), v_one)
+                :
+                    _lat *
+                    (v_s1 + _lat2 *
+                    (v_s2 + _lat2 *
+                    (v_s3 + _lat2 *
+                    (v_s4 + _lat2 *
+                    (v_s5 + _lat2 * (v_s6 + v_s7 * _lat2)))))) + v_one;
+
+                    (v_WGS84_HalfSemiMajorAxis * Vector256.Log(_lat / (v_two - _lat)))
+                      .StoreUnsafe(ref lon_searchSpace, elementOffset);
+
                     elementOffset += elementCountVector256;
-                }
 
-                if (elementOffset < bufferLength)
-                {
-                    min256 = Vector256.Min(min256, Vector256.LoadUnsafe(in searchSpace, oneVectorAwayFromEnd));
+                    if (elementOffset > oneVectorAwayFromEnd) break;
                 }
-
-                Vector128<double> min128 = Vector128.Min(min256.GetLower(), min256.GetUpper());
-                double right = min128[1], left = min128[0];
-                return left < right ? left : right;
             }
             else if (Vector128.IsHardwareAccelerated && bufferLength >= elementCountVector128)
             {
-
-                ref readonly double searchSpace = ref x.GetPinnableReference();
-                Vector128<double> min128 = Vector128.LoadUnsafe(in searchSpace);
-                nuint elementOffset = elementCountVector128;
-                nuint oneVectorAwayFromEnd = bufferLength - elementCountVector128;
-                while (elementOffset <= oneVectorAwayFromEnd)
-                {
-                    min128 = Vector128.Min(min128, Vector128.LoadUnsafe(in searchSpace, elementOffset));
-                    elementOffset += elementCountVector128;
-                }
-
-                if (elementOffset < bufferLength)
-                {
-                    min128 = Vector128.Min(min128, Vector128.LoadUnsafe(in searchSpace, oneVectorAwayFromEnd));
-                }
-
-                double right = min128[1], left = min128[0];
-                return left < right ? left : right;
+               
             }
 
-            // Non-SIMD scalar handling:
-            double min = x[0];
-            for (int i = 1; i < (int)bufferLength;)
+            // Non-SIMD scalar & tail handling:
+            while (elementOffset < bufferLength)
             {
-                double current = x[i++];
-                if (min > current)
-                {
-                    min = current;
-                }
+                ref double scalar = ref Unsafe.Add(ref lon_searchSpace, elementOffset++);
+                scalar = double.DegreesToRadians(scalar);
             }
-            return min;
         }
     }
 }
