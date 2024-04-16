190d95ec9b2640e22b1b4e9c57d5266ec23f3263
Ákos Halmai <halmaia@gmail.com>
2024-04-16 19:36:14 +0200

    Scalar handlin error removed.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿namespace GeoSIMD
 {
-    internal class Program
+    internal static class Program
     {
-
-        static void Main(string[] args)
+        static void Main()
         {
             double[] a = [double.NaN, 40, 0, 99, 1, 2, 3, 4, 7, 2, double.NaN, 40, 0, 99, 1, 2, 3, 4, 7, 2];
             double[] b = new double[a.Length];
             a.CopyTo(b, 0);
-           //GeoSIMD.WGS84_WebMercator(a);
+
+            
+
+            //GeoSIMD.WGS84_WebMercator(a);
+
+
 
         }
     }
